c8d0957e2e76c9dd4110e679a2dded5c0a8119fd
Niraj-Kumar-Chauhan <nirajstudent74880@gmail.com>
2025-03-06 01:34:12 +0530

    search key element in sorted matrix

---
+++ java session 12  2D ARRAY/search_in_sorted_matrix.java
@@ -0,0 +1,26 @@
+public class search_in_sorted_matrix {
+    public static boolean staircaseSearch(int arr[][], int key){
+        int row = 0;
+        int col = arr.length-1;
+        while(row < arr.length && col >= 0){
+            if(key == arr[row][col]){
+                System.out.println("Index no. of key"+"("+key+") is : " + "("+ row + "," + col + ")");
+                return true;
+            }
+            else if(key < arr[row][col]){
+                col--;
+            }
+            else if(key > arr[row][col]){
+                row++;
+            }
+        }
+        System.out.println("The Key("+key+") is not found");
+        return false;
+
+    }
+    public static void main(String args[]){
+        int Matrix[][] = {{10, 20, 30, 40}, {15, 25, 35, 45}, {27, 29, 37, 48}, {32, 33, 39, 50}};
+        int key = 33;
+        staircaseSearch(Matrix, key);  
+    }
+}
