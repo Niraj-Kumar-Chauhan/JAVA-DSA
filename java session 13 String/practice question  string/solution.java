d8ee1e825d66bce5241f6ecad23905d1613b44e0
Niraj-Kumar-Chauhan <nirajstudent74880@gmail.com>
2025-03-12 00:06:00 +0530

    string basic concept

---
+++ java session 13 String/practice question  string/solution.java
@@ -0,0 +1,16 @@
+// public class solution {
+//     public static void main(String args[]) {
+//         String str="ShradhaDidi";
+//         String str1="ApnaCollege";
+//         String str2="ShradhaDidi";
+//         System.out.println(str.equals(str1) + " " + str.equals(str2));
+//     }
+// }
+
+
+public class solution{
+    public static void main(String args[]) {
+        String str = "ApnaCollege".replace("l","f");
+        System.out.println(str);
+    }
+}
