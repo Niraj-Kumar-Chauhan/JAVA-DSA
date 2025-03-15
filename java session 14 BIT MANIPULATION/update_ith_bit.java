ee5496d0f6c2978f79badf389f40edb0a0d354d3
Niraj-Kumar-Chauhan <nirajstudent74880@gmail.com>
2025-03-15 16:44:15 +0530

    update ith position of binary number for a number

--- java session 14 BIT MANIPULATION/update_ith_bit.java
+++ java session 14 BIT MANIPULATION/update_ith_bit.java
@@ -1,36 +1,3 @@
-import java.util.*;
-
-public class update_ith_bit {
-    public static int ClearIthBit(int n , int i){
-        int bitMast = ~(1<<i);
-        return n & bitMast;
-    }
-    public static int SetIthBit(int n , int i){
-        int bitMask = 1<<i;
-        return n | bitMask;
-    }
-    public static int updateIthBit(int n , int i , int bit){
-        if(bit == 1){
-            return SetIthBit(n, i);
-        }else{
-            return n;
-        }
-    }
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter A Number : ");
-        int num = sc.nextInt();
-        System.out.print("Enter Ith No. To  Clear Bit at Ith Position : ");
-        int i = sc.nextInt();
-        System.out.print("Enter 1 For Update Or 0 For Clear : ");
-        int bit = sc.nextInt();
-        
-        System.out.println(updateIthBit(num, i, bit));
-    }
-}
-
-
-
 // import java.util.*;
 
 // public class update_ith_bit {
@@ -38,10 +5,16 @@ public class update_ith_bit {
 //         int bitMast = ~(1<<i);
 //         return n & bitMast;
 //     }
-//     public static int updateIthBit(int n , int i , int bit){
-//         n = ClearIthBit(n, i);
-//         int bitMask = bit<<i;
+//     public static int SetIthBit(int n , int i){
+//         int bitMask = 1<<i;
 //         return n | bitMask;
+//     }
+//     public static int updateIthBit(int n , int i , int bit){
+//         if(bit == 1){
+//             return SetIthBit(n, i);
+//         }else{
+//             return n;
+//         }
 //     }
 //     public static void main(String args[]){
 //         Scanner sc = new Scanner(System.in);
@@ -55,3 +28,30 @@ public class update_ith_bit {
 //         System.out.println(updateIthBit(num, i, bit));
 //     }
 // }
+
+
+
+import java.util.*;
+
+public class update_ith_bit {
+    public static int ClearIthBit(int n , int i){
+        int bitMast = ~(1<<i);
+        return n & bitMast;
+    }
+    public static int updateIthBit(int n , int i , int bit){
+        n = ClearIthBit(n, i);
+        int bitMask = bit<<i;
+        return n | bitMask;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A Number : ");
+        int num = sc.nextInt();
+        System.out.print("Enter Ith No. To  Clear Bit at Ith Position : ");
+        int i = sc.nextInt();
+        System.out.print("Enter 1 For Update Or 0 For Clear : ");
+        int bit = sc.nextInt();
+        
+        System.out.println(updateIthBit(num, i, bit));
+    }
+}
