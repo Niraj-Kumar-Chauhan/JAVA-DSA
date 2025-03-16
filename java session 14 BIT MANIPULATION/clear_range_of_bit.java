9563e03f427f8cbbf55db7f28373dc00ba6571c1
Niraj-Kumar-Chauhan <nirajstudent74880@gmail.com>
2025-03-16 17:46:30 +0530

    clear bit into  binary no. of a number in a range

--- java session 14 BIT MANIPULATION/clear_range_of_bit.java
+++ java session 14 BIT MANIPULATION/clear_range_of_bit.java
@@ -7,6 +7,5 @@ public class clear_range_of_bit {
     }
     public static void main(String args[]){
         System.out.println(ClearInRangeOfBit(10, 2, 4));
-        System.out.println((1<<2) - 1);
     }
 }
