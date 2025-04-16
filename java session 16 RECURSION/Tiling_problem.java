ea1937f5d2bc73c71c5c4ac110b56b3d86f99811
Niraj-Kumar-Chauhan <nirajstudent74880@gmail.com>
2025-04-17 02:01:28 +0530

    how many choice to arrange tiles

--- java session 16 RECURSION/Tiling_problem.java
+++ java session 16 RECURSION/Tiling_problem.java
@@ -14,3 +14,18 @@ public class Tiling_problem {
         return ways;
     }
 }
+
+         // or
+         
+// public class Tiling_problem {
+//     public static void main(String args[]){
+//         System.out.println(tiling(6));
+//     }
+//     public static int tiling(int n){
+//         if(n==0 || n ==1){
+//             return 1;
+//         }
+//         int ways = tiling(n-1) + tiling(n-2);
+//         return ways;
+//     }
+// }
